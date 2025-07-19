8a64d68277e91a3e71aca57e60a75af5a288ea1f
andreapaccagnella94 <andreapacca94@live.it>
2025-07-19 15:53:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("HI DOM");
+
+//identifico l'immagine con la lampadina spenta
+const lampOffEl = document.getElementById("lamp-off");
+console.log(lampOffEl);
+
+
+//identifico l'immagine con la lampadina accesa
+const lampOnEl = document.getElementById("lamp-on");
+console.log(lampOnEl);
+
+//identifico il bottone
+const buttonEl = document.querySelector("button");
+console.log(buttonEl);
